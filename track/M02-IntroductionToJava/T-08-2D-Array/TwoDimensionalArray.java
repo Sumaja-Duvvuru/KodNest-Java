af672c74e78cee18ee54c0170e4d7a7ef40c9818
Duvvuru Sumaja <duvvurusumaja@gmail.com>
2026-08-19 13:14:48 +0530

    Topic-8 2D Arrays

---
+++ track/M02-IntroductionToJava/T-08-2D-Array/TwoDimensionalArray.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class TwoDimensionalArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a[][] = new int[3][5];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("2D Array elements are: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
